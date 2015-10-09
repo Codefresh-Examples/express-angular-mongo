41102670cc08b7b4caaa1a0d55c17c8d7fa30c1f
raziel <Raziel@codefresh.net>
2015-10-08 19:50:23 -0700

    fix unexpected token

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@
 //in case mongodb installed locally
 //var mongourl = process.env.MONGO_URI? process.env.MONGO_URI : 'mongodb://localhost:27017/exampleDb';
 
-..
+
 var mongourl = process.env.MONGO_URI? process.env.MONGO_URI : 'mongodb://mongo:27017/exampleDb';
 
 module.exports = {
