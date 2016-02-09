df319c17a4ce52c4e19b570b1016ed499e4e615d
noamt <noam@10ne.org>
2016-02-09 11:36:37 +0200

    Adding a dockerfile as a replacement for the reliance on buildpacks

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
-FROM node:onbuild
+FROM davidsblog/node-mongo:latest
 
-EXPOSE 9000
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+ADD ./package.json /usr/src/app/package.json
+RUN npm install
+
+ADD . /usr/src/app
+
+ADD start.sh /usr/src/app/start.sh
+RUN chmod +x /usr/src/app/start.sh
+CMD "/usr/src/app/start.sh"
--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
 /*eslint-env node */
 
-//in case mongodb installed locally
-//var mongourl = process.env.MONGO_URI? process.env.MONGO_URI : 'mongodb://localhost:27017/exampleDb';
+var mongourl = process.env.MONGO_URI? process.env.MONGO_URI : 'mongodb://localhost:27017/exampleDb';
 
 
-var mongourl = process.env.MONGO_URI? process.env.MONGO_URI : 'mongodb://mongo:27017/exampleDb';
+//var mongourl = process.env.MONGO_URI? process.env.MONGO_URI : 'mongodb://mongo:27017/exampleDb';
 
 module.exports = {
 	// mongo database connection url
